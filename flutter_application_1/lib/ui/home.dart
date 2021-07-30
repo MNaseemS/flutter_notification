924c7366a0a960c92374c6252fff05501e0d7937
MNaseemS <64322904+MNaseemS@users.noreply.github.com>
2021-07-30 03:15:04 -0700

    Update home.dart

--- flutter_application_1/lib/ui/home.dart
+++ flutter_application_1/lib/ui/home.dart
@@ -23,48 +23,71 @@ class _HomeState extends State<Home> {
             onPressed: null, icon: new Icon(Icons.menu, color: Colors.white)),
       ),
       backgroundColor: Color(0xFF303030),
-      body: new Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          width: MediaQuery.of(context).size.width * 0.6,
-          alignment: Alignment.center,
-          color: Colors.white,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.notifications),
-                Text("1", style: TextStyle(color: Colors.black)),
-                Row(children: <Widget>[
-                  Text("Push notifications"),
-                  Switch(
-                    value: isSwitched,
-                    onChanged: (value) {
-                      setState(() {
-                        isSwitched = value;
-                      });
-                    },
-                    activeTrackColor: Colors.grey.shade200,
-                    activeColor: Colors.grey.shade400,
-                  )
-                ]),
-                Row(children: <Widget>[
-                  Text("Email notifications"),
-                  Switch(
-                    value: isSwitched2,
-                    onChanged: (value) {
-                      setState(() {
-                        isSwitched2 = value;
-                      });
-                    },
-                    activeTrackColor: Colors.grey.shade200,
-                    activeColor: Colors.grey.shade400,
-                  )
-                ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(onPressed: null, child: Text("Show"))
-                    ])
-              ])),
+      body: Container(
+          alignment: Alignment.topCenter,
+          child: new Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.9,
+              color: Colors.white,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      flex: 6,
+                      child: Container(
+                          alignment: Alignment.center,
+                          decoration: new BoxDecoration(
+                              color: Colors.grey, shape: BoxShape.circle),
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.notifications),
+                              Text("1", style: TextStyle(color: Colors.black))
+                            ],
+                          )),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Row(children: <Widget>[
+                        Text("Push notifications"),
+                        Switch(
+                          value: isSwitched,
+                          onChanged: (value) {
+                            setState(() {
+                              isSwitched = value;
+                            });
+                          },
+                          activeTrackColor: Colors.grey.shade200,
+                          activeColor: Colors.grey.shade400,
+                        )
+                      ]),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Row(children: <Widget>[
+                        Text("Email notifications"),
+                        Switch(
+                          value: isSwitched2,
+                          onChanged: (value) {
+                            setState(() {
+                              isSwitched2 = value;
+                            });
+                          },
+                          activeTrackColor: Colors.grey.shade200,
+                          activeColor: Colors.grey.shade400,
+                        )
+                      ]),
+                    ),
+                    Flexible(
+                        flex: 1,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              TextButton(onPressed: null, child: Text("Show"))
+                            ]))
+                  ]))),
     );
   }
 }
